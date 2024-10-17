3837262e0c21b605645da1ac535b41cade402d3b
agrawalvaibhavbandhan.2005@gmail <agrawalvaibhavbandhan.2005@gmail.com>
2024-10-18 00:32:06 +0530

    Fixed login page

---
+++ js/login.js
@@ -0,0 +1,10 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    // Send data to backend
+
+
+    window.location = "/";
+});
